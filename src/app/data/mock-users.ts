2d42d0cd731d946714a55778af1cbbc90d42a344
tim <tim@ioplus.de>
2024-05-16 17:20:10 +0200

    add 21 more mockdata for first feed pages, update mock user data

--- src/app/data/mock-users.ts
+++ src/app/data/mock-users.ts
@@ -1,23 +1,25 @@
 import { User } from "./user";
 
 export const USERS: User[] = [
-    {id: 1, name: 'koelleforniadreaming', link:'/user/koelleforniadreamin', count: 2},
-    {id: 2, name: 'anliberant', link:'/user/anliberant', count: 1},
-    {id: 3, name: 'smalltownwitch', link:'/user/smalltownwitch', count: 1},
-    {id: 4, name: 'hypetype', link:'/user/hypetype', count: 1,},
-    {id: 5, name: 'charlienoir',link: '/user/charlienoir' ,count: 513 },
-    {id: 6, name: 'piltdown',link: '/user/piltdown' ,count: 2870 },
-    {id: 7, name: 'jess',link: '/user/jess' ,count: 5993 },
-    {id: 8, name: 'turnheim-collette',link: '/user/turnheim-collette' ,count: 1174 }, 
-    {id: 9, name: 'talfad',link: '/user/talfad' ,count: 8 },
-    {id: 10, name: 'er-minio',link: '/user/er-minio' ,count: 890 },
-    {id: 11, name: 'jeffhamada',link: '/user/jeffhamada' ,count: 12020 },
-    {id: 12, name: 'radiostaticstar',link: '/user/radiostaticstar' ,count: 399 },
-    {id: 13, name: 'nacionjaguar',link: '/user/nacionjaguar' ,count: 13 },
-    {id: 14, name: 'brandpowder',link: '/user/brandpowder' ,count: 11817 },
-    {id: 15, name: 'visualaffection',link: '/user/visualaffection' ,count: 4145 },
-    {id: 16, name: 'mrpt',link: '/user/mrpt' ,count: 29869 },
-    {id: 17, name: 'newd3',link: '/user/newd3' ,count: 8551 },
-    {id: 18, name: 'ricin',link: '/user/ricin' ,count: 3667 },
-    {id: 19, name: 'peekasso',link: '/user/peekasso' ,count: 5003 }
-  ];
+    {id: 1, name: 'koelleforniadreaming', link:'/user/koelleforniadreamin', count: 9},
+    {id: 2, name: 'anliberant', link:'/user/anliberant', count: 6},
+    {id: 3, name: 'smalltownwitch', link:'/user/smalltownwitch', count: 3},
+    {id: 4, name: 'hypetype', link:'/user/hypetype', count: 7},
+    {id: 5, name: 'charlienoir',link: '/user/charlienoir', count: 3}
+  ]
+    /*;,
+    {id: 6, name: 'piltdown',link: '/user/piltdown', count: 0},
+    {id: 7, name: 'jess',link: '/user/jess', count: 0},
+    {id: 8, name: 'turnheim-collette',link: '/user/turnheim-collette', count: 0}, 
+    {id: 9, name: 'talfad',link: '/user/talfad', count: 0},
+    {id: 10, name: 'er-minio',link: '/user/er-minio', count: 0},
+    {id: 11, name: 'jeffhamada',link: '/user/jeffhamada', count: 0},
+    {id: 12, name: 'radiostaticstar',link: '/user/radiostaticstar', count: 0},
+    {id: 13, name: 'nacionjaguar',link: '/user/nacionjaguar', count: 0},
+    {id: 14, name: 'brandpowder',link: '/user/brandpowder', count: 0},
+    {id: 15, name: 'visualaffection',link: '/user/visualaffection', count: 0},
+    {id: 16, name: 'mrpt',link: '/user/mrpt' , count: 0},
+    {id: 17, name: 'newd3',link: '/user/newd3', count: 0},
+    {id: 18, name: 'ricin',link: '/user/ricin', count: 0},
+    {id: 19, name: 'peekasso',link: '/user/peekasso' ,count: 0}
+  ];*/
