e092da1af73e713c518e29a4993413800ebc6d42
tim <tim@ioplus.de>
2024-05-23 10:23:00 +0200

    #2 fix links to user feeds

--- src/app/useractivity/useractivity.component.ts
+++ src/app/useractivity/useractivity.component.ts
@@ -1,12 +1,14 @@
 import { User } from "../data/user";
 import { USERS } from "../data/mock-users";
 import { Component } from "@angular/core";
+import { RouterLink } from '@angular/router';
 
 @Component(
     {
         selector: 'user-activity-root',
         standalone: true,
-        templateUrl: './useractivity.component.html'
+        templateUrl: './useractivity.component.html',
+        imports:[RouterLink]
     })
 
 export class UserActivityComponent
