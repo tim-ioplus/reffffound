f5003069527e72e842c775946669b809c9f1f800
tim-ioplus <138361783+tim-ioplus@users.noreply.github.com>
2024-10-17 15:33:28 +0200

    adding docker, debug snippet for db path in programm.cs

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,39 @@
-#See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
-
+# Base stage: Use ASP.NET runtime
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
-USER app
 WORKDIR /app
 EXPOSE 8080
-EXPOSE 8081
 
+# Build stage: Build the service project
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
+
+# Copy the project file and restore dependencies
 COPY ["reffffound.csproj", "."]
 RUN dotnet restore "./reffffound.csproj"
-COPY . .
-WORKDIR "/src/."
-RUN dotnet build "./reffffound.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
+# Copy the remaining files and build the app
+COPY . .
+RUN dotnet build "./reffffound.csproj" -c ${BUILD_CONFIGURATION} -o /app/build
+
+# copy sqlite db
+# COPY app.db /app/
+# RUN chmod 777 /app/app.db
+
+# Publish stage: Publish the service project
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
-RUN dotnet publish "./reffffound.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+RUN dotnet publish "./reffffound.csproj" -c ${BUILD_CONFIGURATION} -o /app/publish /p:UseAppHost=false
 
+# Final stage: Run the app
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "reffffound.csproj"]
+
+# Copy the app.db file from the output directory of the build
+# Make sure this path matches the actual output path
+#COPY bin/Release/net8.0/app.db /app/app.db
+COPY app.db /app/app.db
+RUN chmod 777 /app/app.db
+
+ENTRYPOINT ["dotnet", "reffffound.dll"]
--- Program.cs
+++ Program.cs
@@ -3,57 +3,117 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Protocols.Configuration;
 using reffffound.Data;
+using reffffound.Models;
 using reffffound.Services;
 using System.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder( args );
 
 // Set Up connection contexts 
-var connectionString = builder.Configuration.GetConnectionString("DataConnection") ?? throw new InvalidOperationException("Default Connection string 'DataConnection' not found.");
-if(string.IsNullOrEmpty(connectionString)) throw new InvalidConfigurationException("Data Connection String is missing");
+//var connectionString = builder.Configuration.GetConnectionString("DataConnection") ?? throw new InvalidOperationException("Default Connection string 'DataConnection' not found.");
+//if(string.IsNullOrEmpty(connectionString)) throw new InvalidConfigurationException("Data Connection String is missing");
 
-var applicationDBConnectionString = builder.Configuration.GetConnectionString("ApplicationDBConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDBConnection' not found.");
-if(string.IsNullOrEmpty(applicationDBConnectionString)) throw new InvalidConfigurationException("ApplicationDBConnection Connection String is missing");
+//var applicationDBConnectionString = builder.Configuration.GetConnectionString("ApplicationDBConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDBConnection' not found.");
+//if(string.IsNullOrEmpty(applicationDBConnectionString)) throw new InvalidConfigurationException("ApplicationDBConnection Connection String is missing");
+
+bool isDocker = System.IO.File.Exists( "/proc/1/cgroup" );
+string connectionKey = isDocker ? "DockerApplicationLocalDBConnection" : "ApplicationLocalDBConnection";
+var applicationLocalDBConnectionString = builder.Configuration.GetConnectionString( connectionKey ) ?? throw new InvalidOperationException( "Connection string 'ApplicationDBConnection' not found." );
+if (string.IsNullOrEmpty( applicationLocalDBConnectionString )) throw new InvalidConfigurationException( connectionKey + " Connection String is missing" );
+
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(applicationDBConnectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(applicationDBConnectionString));
+builder.Services.AddDbContext<ApplicationDbContext>( options => options.UseSqlite( applicationLocalDBConnectionString ) );
+builder.Services.AddDatabaseDeveloperPageExceptionFilter( );
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>( options => options.SignIn.RequireConfirmedAccount = false )
+	 .AddEntityFrameworkStores<ApplicationDbContext>( ).AddDefaultTokenProviders( );
 
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IBookmarkService, BookmarkService>();
+builder.Services.AddScoped<IUserService, UserService>( );
+builder.Services.AddScoped<IBookmarkService, BookmarkContextService>( );
+builder.Services.AddSingleton( builder.Configuration );
 
-builder.Services.AddSingleton(builder.Configuration);
+builder.Services.AddControllersWithViews( );
 
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
+var app = builder.Build( );
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment( ))
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint( );
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler( "/Home/Error" );
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts( );
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection( );
+app.UseStaticFiles( );
 
-app.UseRouting();
-
-app.UseAuthorization();
-
+app.UseRouting( );
+app.UseAuthorization( );
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Bookmarks}/{action=Index}/{id?}");
-app.MapRazorPages();
+	 name: "default",
+	 pattern: "{controller=Bookmarks}/{action=Index}/{id?}" );
+app.MapRazorPages( );
 
-app.Run();
+// 
+if (true || isDocker)
+{
+	using (var scope = app.Services.CreateScope( ))
+	{
+		Console.WriteLine("Working in " + Environment.CurrentDirectory);
+		var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>( );
+		var filelist = new List<string>()
+		{
+			"app.db", "/app.db", "//app.db/"
+		};
+
+		var pathList = new List<string>()
+		{
+			"", "app", "app/", "app//"
+		};
+
+		foreach ( var path in pathList )
+		{
+			foreach ( var file in filelist )
+			{
+				var filepath = path + file;
+				if(File.Exists(filepath))
+				{
+					Console.WriteLine("Exists: " +  filepath);
+				}
+				else
+				{
+					Console.WriteLine("Not Exists: " + filepath);
+				}
+			}
+		}
+
+		
+
+		if(dbContext.Database.CanConnect())
+		{
+			Console.WriteLine("SUCCESS: database app.db IS available at " + applicationLocalDBConnectionString);
+
+			if(!dbContext.Bookmarks.Any())
+			{
+				Console.WriteLine("No bookmarks in _data");
+			}
+			else
+			{
+				int count = dbContext.Bookmarks.Count();
+				Console.WriteLine(count + " bookmarks in _data");
+			}
+		}
+		else
+		{
+			Console.WriteLine("ERROR: database app.db IS NOT available at " + applicationLocalDBConnectionString);
+		}
+	} 
+}
+
+app.Run( );
